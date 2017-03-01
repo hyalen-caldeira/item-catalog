cd0fd944496a32f28eda8c580381c90f39936b0d
Hyalen Moreira <hyalen.moreira@gmail.com>
2017-02-28 19:13:34 -0800

    Add JSON functionality

--- Flask App Example/database_setup.py
+++ Flask App Example/database_setup.py
@@ -1,11 +1,5 @@
-# Database setup consist of:
-#   . Import the libraries and configure
-#   . Class and table definition
-#   . Mapping definition
-
-### Configuration
-import sys
 import os
+import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
@@ -13,27 +7,42 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-### Class Definition
-
 class Restaurant(Base):
-    ### Table Definition
     __tablename__ = 'restaurant'
-    ## Columns
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
 
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-    ## Columns
-    id = Column(Integer, primary_key = True)
-    name = Column(String(80), nullable = False)
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship('Restaurant')
+    restaurant = relationship(Restaurant)
 
-### Create the DB
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
 engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metadata.create_all(engine)
 
+Base.metadata.create_all(engine)
